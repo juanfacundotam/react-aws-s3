93e968619d0f83aff4ce12be3f4ba237e6696d8f
Juan Facundo Tam <107519577+juanfacundotam@users.noreply.github.com>
2023-12-27 10:28:32 -0300

    Update App.jsx

--- client-aws-s3/src/App.jsx
+++ client-aws-s3/src/App.jsx
@@ -6,14 +6,17 @@ const [post, setPost] = useState({
   title:'',
   photo: null
 })
-const handleSubmit = () => {
+const handleSubmit = async (e) => {
+  e.preventDefault();
   const formData = new FormData();
   formData.append('photo', post.photo);
-  axios.post('http://localhost:3000/upload', formData, {
+  const response = await axios.post('http://localhost:3000/upload', formData, {
     headers: {
       "Content-Type": "multipart/form-data"
     }
   })
+
+  console.log(response)
 }
 
   return (
