dc63d81316f8f74ae58c9f80ca6ef366bb84aa7c
Juan Facundo Tam <107519577+juanfacundotam@users.noreply.github.com>
2023-12-27 17:24:16 -0300

    Update photos.routes.js

--- server-aws-s3/photos.routes.js
+++ server-aws-s3/photos.routes.js
@@ -1,11 +1,26 @@
 const { Router } = require("express");
-
+const { uploadFile, readFile } = require("./s3");
 const router = Router();
 
 router.get("/", (req, res) => res.send("Welcome to server"));
-router.post("/upload", (req, res) => {
-    console.log(req.files)
-    res.send("File Uploaded")
+
+router.post("/upload", async (req, res) => {
+  // console.log(req.files["photo"].tempFilePath)
+  const result = await uploadFile(req.files["photo"]);
+
+  res.send("File Uploaded");
+
+  console.log(result);
+});
+
+router.get("/archivo/:fileName", async (req, res) => {
+    try {
+        const result = await readFile(req.params.fileName);
+        res.send("Tu Archivo fue descargado");
+    } catch (error) {
+        // Maneja el error aquí, ya que readFile ahora lanza errores si ocurren
+        console.log("Tu Archivo no fue descargado");
+    }
 });
 
 module.exports = router;
