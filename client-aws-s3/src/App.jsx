3bdd38d3c334f65eb3ace02da28cb80107d1028a
Juan Facundo Tam <107519577+juanfacundotam@users.noreply.github.com>
2023-12-27 09:56:34 -0300

    Create App.jsx

---
+++ client-aws-s3/src/App.jsx
@@ -0,0 +1,30 @@
+import { useState } from "react"
+import axios from "axios";
+
+function App() {
+const [post, setPost] = useState({
+  title:'',
+  photo: null
+})
+const handleSubmit = () => {
+  const formData = new FormData();
+  formData.append('photo', post.photo);
+  axios.post('http://localhost:3000/upload', formData, {
+    headers: {
+      "Content-Type": "multipart/form-data"
+    }
+  })
+}
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input type="text" placeholder="title" onChange={e => setPost({...post, title: e.target.value})}/>
+        <input type="file" name="photo" onChange={e => setPost({...post, photo: e.target.files[0]})}/>
+        <button>Subir</button>
+      </form>
+    </div>
+  )
+}
+
+export default App
