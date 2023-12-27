48cb220adff60ec6ab1d9393bfb4b21523d4c5ef
Juan Facundo Tam <107519577+juanfacundotam@users.noreply.github.com>
2023-12-27 09:56:53 -0300

    Create photos.routes.js

---
+++ server-aws-s3/photos.routes.js
@@ -0,0 +1,11 @@
+const { Router } = require("express");
+
+const router = Router();
+
+router.get("/", (req, res) => res.send("Welcome to server"));
+router.post("/upload", (req, res) => {
+    console.log(req.files)
+    res.send("File Uploaded")
+});
+
+module.exports = router;
